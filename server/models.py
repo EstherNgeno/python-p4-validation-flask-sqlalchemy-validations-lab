bc2bdd76d9970fd99590cc869faf81cc6ffec1af
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-11-22 18:22:34 +0300

    final commit

--- server/models.py
+++ server/models.py
@@ -12,6 +12,29 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates("name")
+    def validate_name(self, key, name):
+        if len(name) < 3 or len(name) > 50:
+            raise ValueError("Name must contain only alphabetic characters, spaces, or hyphens.")
+        if db.session.query(Author).filter_by(name=name).first():
+            raise ValueError("Name must be unique.")
+        return name
+    
+    @validates("phone_number")
+    def validate_phone_numnber(self, key, phone_number):
+        if len(phone_number) !=10:
+            raise ValueError("Phone number must be eaxctly 10 digits.")
+        if not phone_number.isdigit():
+            raise ValueError("Phone number must contain only 10 digits.")
+        return phone_number
+    
+    @validates('id')
+    def validate_id(self, key, id):
+        if id <= 0:
+            raise ValueError('ID must be a positive integer.')
+        if id > 10000:
+            raise ValueError('ID must be less than or equal to 10000.')
+        return id
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +51,33 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates("content")
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return content
+    
+    @validates("summary")
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary must be a maximum of 250 characters long.")
+        return summary
+    
+    @validates("category")
+    def validates_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
 
+        return category
+    
+    @validates("title")
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in  title for phrase in clickbait_phrases):
+            raise ValueError('Title must contain one of the following phrases: {", ".join(clickbait_phrases)}.')
+        return title
+    
+    
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
